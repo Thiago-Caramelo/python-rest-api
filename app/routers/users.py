c13c429d390310b4edd0fd684294c1c0a219d3b6
Thiago Caramelo <thiago.pescado@gmail.com>
2020-06-04 18:20:34 -0300

    Users route

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,8 +1,21 @@
-from fastapi import APIRouter
+from typing import List
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends
+from ..database.database import SessionLocal
+from ..database import schemas, crud
 
 router = APIRouter()
 
 
-@router.get("/users/", tags=["users"])
-async def read_users():
-    pass
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.get("/users/", tags=["users"], response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
