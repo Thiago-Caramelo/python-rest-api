4d70813f584706313449419aa2db4014abb1df13
Thiago Caramelo <thiago.pescado@gmail.com>
2020-06-05 10:55:38 -0300

    Debug settings

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import Depends, FastAPI, Header, HTTPException
 from app.routers import users
 from app.database import models
@@ -8,3 +9,6 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(users.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
