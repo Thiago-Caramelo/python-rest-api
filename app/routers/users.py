eab316d2bff8be926dda80e97237a3989835387c
Thiago Caramelo <thiago.pescado@gmail.com>
2020-06-08 11:02:52 -0300

    Refactor database session dependency

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,21 +1,12 @@
+import app.database.session as session
 from typing import List
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends
-from ..database.database import SessionLocal
 from ..database import schemas, crud
-
 router = APIRouter()
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
 @router.get("/users/", tags=["users"], response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(session.get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
