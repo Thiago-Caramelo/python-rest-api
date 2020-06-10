4b8e26a82ab2c9e01a6f593ce2127e8632c6cba6
Thiago Caramelo <thiago.pescado@gmail.com>
2020-06-10 16:56:42 -0300

    Unit tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from main import app
+from json import loads
 
 client = TestClient(app)
 
@@ -7,3 +8,4 @@ client = TestClient(app)
 def test_get_users():
     response = client.get("/users/")
     assert response.status_code == 200
+    assert loads(response.text) != None
