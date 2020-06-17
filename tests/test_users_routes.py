bac08170aa710804b2058d1fb39224d60a390da3
Thiago Caramelo <thiago.pescado@gmail.com>
2020-06-17 11:25:13 -0300

    Refactor imports

--- tests/test_users_routes.py
+++ tests/test_users_routes.py
@@ -1,10 +1,10 @@
 import app.routers.users as users
-from mock import patch
-from uuid import uuid4
+import mock
+import uuid
 import pytest
-from fastapi import HTTPException
-from app.schemas.user import UserSchema
-from app.database.models.user import User
+import fastapi
+import app.schemas.user as user_schema
+import app.database.models.user as user_database
 
 users_result = [{"name": "name goes here"}]
 user_result = {"user": "name"}
@@ -15,7 +15,7 @@ def get_users_mock(db, skip, limit):
 
 
 def test_read_users():
-    with patch('app.routers.users.user', autospec=True) as mock_module:
+    with mock.patch('app.routers.users.user', autospec=True) as mock_module:
         mock_module.get_users.return_value = users_result
         result = users.read_users(skip=0, limit=0, db=None)
         assert result == users_result
@@ -23,28 +23,28 @@ def test_read_users():
 
 
 def test_read_user():
-    uuid4_id = uuid4()
-    with patch.object(users.user, 'get_user', return_value=user_result) as mock_method:
+    uuid4_id = uuid.uuid4()
+    with mock.patch.object(users.user, 'get_user', return_value=user_result) as mock_method:
         result = users.read_user(uuid4_id, db=None)
         assert result == user_result
     mock_method.assert_called_once_with(None, user_id=uuid4_id)
 
 
 def test_read_user_not_found():
-    uuid4_id = uuid4()
-    with patch('app.routers.users.user', autospec=True) as mock_module:
+    uuid4_id = uuid.uuid4()
+    with mock.patch('app.routers.users.user', autospec=True) as mock_module:
         mock_module.get_user.return_value = None
-        with pytest.raises(HTTPException):
+        with pytest.raises(fastapi.HTTPException):
             result = users.read_user(uuid4_id, db=None)
     mock_module.get_user.assert_called_once_with(None, user_id=uuid4_id)
 
 
 def test_create_user():
-    with patch('app.routers.users.user', autospec=True) as mock_module:
-        user_id = uuid4()
-        new_user = UserSchema(
+    with mock.patch('app.routers.users.user', autospec=True) as mock_module:
+        user_id = uuid.uuid4()
+        new_user = user_schema.UserSchema(
             id=user_id, email="test@test.com", is_active=True)
-        new_database_user = User(
+        new_database_user = user_database.User(
             id=user_id, email="test@test.com", is_active=True)
         mock_module.get_user_by_email.return_value = None
         mock_module.create_user.return_value = new_database_user
@@ -55,11 +55,11 @@ def test_create_user():
 
 
 def test_create_user_exists():
-    with patch('app.routers.users.user', autospec=True) as mock_module:
-        existing_user = User(
-            id=uuid4(), email="test@test.com", is_active=True)
-        new_user = UserSchema(
-            id=uuid4(), email="test@test.com", is_active=True)
+    with mock.patch('app.routers.users.user', autospec=True) as mock_module:
+        existing_user = user_database.User(
+            id=uuid.uuid4(), email="test@test.com", is_active=True)
+        new_user = user_schema.UserSchema(
+            id=uuid.uuid4(), email="test@test.com", is_active=True)
         mock_module.get_user_by_email.return_value = existing_user
-        with pytest.raises(HTTPException):
+        with pytest.raises(fastapi.HTTPException):
             users.create_user(newUser=new_user, db=None)
